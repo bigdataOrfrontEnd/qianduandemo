23ff574c994d7d38ce7a309b63daabb9f86997e6
鄢牧园 <yanmuyuan@dedibot.com>
2022-11-01 10:10:46 +0800

    完成

--- form表单验证/script.js
+++ form表单验证/script.js
@@ -17,3 +17,58 @@ function showSuccess(input){
     formControl.className='form-control success';
 }
 
+function checkEmail(input){
+    const re=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    }else{
+        showError(input,'Email is not valid')
+    }
+}
+function checkRequired(inputArr){
+    let isRequired=false;
+    inputArr.forEach(function(input){
+        if(input.value.trim()==''){
+            showError(input,`${getFieldName(input)} is required`);
+            isRequired=true;
+        }else{
+            showSuccess(input)
+        }
+    })
+    return isRequired
+}
+
+function checkLength(input,min,max){
+    if(input.value.length<min){
+        showError(
+            input,
+            `${getFieldName(input)} must be at least ${min} characters`
+        )
+    }else if(input.value.length>max){
+        showError(input,`${getFieldName(input)} must be at least ${max} characters`);
+    }else{
+        showSuccess(input);
+    }
+}
+function checkPasswordsMatch(input1,input2){
+    if(input1.value!==input2.value){
+        showError(input2, 'Passwords do not match');   
+    }
+}
+function getFieldName(input){
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+// Event listeners
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    if(checkRequired([username, email, password, password2])){
+      checkLength(username, 3, 15);
+      checkLength(password, 6, 25);
+      checkEmail(email);
+      checkPasswordsMatch(password, password2);
+    }
+  
+  });
+
+
